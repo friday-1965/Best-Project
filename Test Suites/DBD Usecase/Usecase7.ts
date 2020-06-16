342f7aee05cb3c6c5435c15ee4a126c2b4ba6bbc
WIN10 <WIN10@DESKTOP-0NVCT6K>
2020-06-17 00:26:22 +0700

    Add DBDTest Suite

---
+++ Test Suites/DBD Usecase/Usecase7.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Usecase7</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5996f90a-39f9-4b63-b998-d4e9e6de1c1d</testSuiteGuid>
+   <testCaseLink>
+      <guid>f54efbcf-31e2-43e8-8d73-6bd5867daaeb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DBD Test Case/1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f2249346-9c2c-412c-9995-0abf8df2b0dd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DBD Test Case/13</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/DBD Usecase/Usecase8.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Usecase8</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>72ad9738-94b9-4d6d-bfd4-41555c5c69bc</testSuiteGuid>
+   <testCaseLink>
+      <guid>932634ee-9714-43a2-a40a-3e318d8a593c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DBD Test Case/1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>64c5c53e-1d0c-411a-a253-5fc1bfdf0482</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DBD Test Case/14</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
